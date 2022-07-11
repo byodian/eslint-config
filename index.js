6f23a8ae15a2fbfb1adee737eefc51f73ffa7c9d
byodian <byj6696@gmail.com>
2022-07-11 17:42:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-prop-types': 'off',
+  },
+};
